f71da8a270e2f1abf2617d49a45f40beee6b72e6
ayusxh009 <1raj.aayush@gmail.com>
2025-01-30 00:01:49 +0530

    minor

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,13 +1,14 @@
-import { SignIn } from '@clerk/nextjs'
-import { div } from 'framer-motion/dist/client';
+import { SignIn } from '@clerk/nextjs';
+import { motion } from 'framer-motion';
+import Header from '../../Header';
 
 export default function Page() {
-
   return (
-
-        <div className="flex items-center justify-center h-full ">
-            <SignIn afterSignInUrl="/Main"/>
-      
-    </div>
+    <>
+      <Header />
+      <div className="flex items-center justify-center h-full py-10">
+        <SignIn afterSignInUrl="/Main" />
+      </div>
+    </>
   );
 }
